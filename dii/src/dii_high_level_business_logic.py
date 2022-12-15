9714dcc33780ba9fecf834aa35bba599ad102000
gaohn <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-12-15 16:30:40 +0800

    docs: update module docstring of dii/src/dii_high_level_business_logic.py

--- dii/src/dii_high_level_business_logic.py
+++ dii/src/dii_high_level_business_logic.py
@@ -1,6 +1,11 @@
 """This module is the high-level business logic of the project. This module will only
 depend on the abstract interface module. This module will not depend on any low-level
-concrete implementations."""
+concrete implementations.
+
+Contrary to the violation example earlier, we have successfully decoupled the
+**injected** our dependency (the original ImageClassificationTransforms) as
+an abstract interface (Transforms).
+"""
 from typing import Any
 from src.dii_base import Transforms  # from abstract interface import Transforms
 
